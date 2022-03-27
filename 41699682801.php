3003d8c9a84d4fa16c7d707b538d0bee715c94f3
Ricardo <ricardoxsobral@gmail.com>
2022-03-26 21:17:08 -0300

    all files

---
+++ 41699682801.php
@@ -0,0 +1,170 @@
+<!--
+    Ricardo de Oliveira Sobral Pinto
+    ricardoxsobral@gmail.com
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css" media="screen" />
+    <title>Teste Prático - O novo mercado</title>
+</head>
+<body>
+    <header>
+        <div>
+            <div class="contador">
+                <div class="text1 col-3">
+                    <p>Essa oferta acaba em:</p>
+                </div>
+                <div class="time col-3">
+                    <div class="day">
+                        <p class="number">00</p><p class="days">dias</p>
+                    </div>
+                    <div class="hr">
+                        <p class="number">00</p><p class="days">horas</p>
+                    </div>
+                    <div class="min">
+                        <p class="number">00</p><p class="days">minutos</p>
+                    </div>
+                    <div class="sec">
+                        <p class="number">00</p><p class="days">segundos</p>
+                    </div>
+                </div>
+                <div class="text2 col-3">
+                    <p>Ou enquanto durarem os estoques</p>
+                </div>
+            </div>
+            <div class="banner-image">
+                <img src="assets/book1mobile.png">
+            </div>
+            <div class="inicio">
+                <div class="title1">
+                    <p>Não existe marketing sem uma história de amor.</p>
+                </div>
+                <div class="subtitle1">
+                    <p>Aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula,</p>
+                </div>
+                <div class="button">
+                    <label class="button-buy-now-1">COMPRE AGORA</label>
+                </div>
+            </div>
+        <div>        
+    </header>
+<!--
+    <section>
+        <div>
+            <div class="videos">
+                
+            </div>
+            <div class="title2">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+            </div>
+            <div class="subtitle2">
+                <p>Aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula,</p>
+            </div>
+            <div class="button-buy-now-2"></div>
+            <div>
+                <div class="img1">
+                    <img src="assets/book2.png">
+                </div>
+                <div class="text2">
+                    <div class="title3">
+                        <h1>Lorem Ipsum dolor site amet.</h1>
+                    </div>
+                    <div class="subtitle3">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales, enim eget suscipit feugiat, risus ipsum imperdiet leo, et euismod tortor nibh vitae orci. </p> 
+                        <p>Mauris fermentum dui vitae turpis mollis, aliquam suscipit urna euismod. Ut porttitor iaculis venenatis.</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="text3">
+                    <div class="title4">
+                        <h1>Lorem Ipsum dolor site amet.</h1>
+                    </div>
+                    <div class="subtitle4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales, enim eget suscipit feugiat, risus ipsum imperdiet leo, et euismod tortor nibh vitae orci.</p>
+                        <p>Mauris fermentum dui vitae turpis mollis, aliquam suscipit urna euismod. Ut porttitor iaculis venenatis.</p>
+                    </div>
+                </div>
+                <div class="img2">
+                    <img src="assets/book3.png">
+                </div>
+            </div>
+            <div>
+                <div class="img3">
+                    <img src="assets/woman.png">
+                </div>
+                <div class="text4">
+                    <div class="title5">
+                        <h1>Bernadette Jiwa</h1>
+                    </div>
+                    <div class="subtitle5">
+                        <p>Aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula,</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="text5">
+                    <div class="title6">
+                        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                    </div>
+                    <div class="subtitle6">
+                        <p>Aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula,</p>
+                    </div>
+                </div>
+                <div class="img4"></div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <div class="title7">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+            <div class="subtitle7">Aenean eget ultrices ipsum. Pellentesque quis laoreet massa. Mauris sed porta tellus, nec euismod est. Praesent velit ligula,</div>
+            <div class="slider1"></div>
+            <div class="price">
+                <div class="wrong-price"><div>
+                <div class="real-price"><div>
+                <div class="button-buy-now-3"><div>
+                <div class="options"><div>
+                <div class="subtitle8">
+                    <p>Frete grátis para todo o Brasil</p>
+                <div>
+            <div>
+            <div class="slider2">
+                <div class="option1"><div>
+                <div class="option2"><div>
+                <div class="option3"><div>
+            <div>
+            <div class="common-questions">
+                <div class="title8">
+                    <h1>Perguntas frequentes</h1>
+                </div>
+                <div class="questions">
+                    <div class="question1">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+                    </div>
+                    <div class="question2">
+                        <p>Donec mattis sodales nunc, ac faucibus libero iaculis in?</p>
+                    </div>
+                    <div class="question3">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p> 
+                    </div>
+                    <div class="question4">
+                        <p>Vestibulum dignissim placerat ex in hendrerit. Donec eu sollicitudin arcu?</p>
+                    </div>
+                    <div class="question5">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+                    </div>
+                    <div class="question6">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </section>
+</body>
+</html>
